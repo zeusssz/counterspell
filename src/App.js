18375ef2a060693737996562e65b390531cfefff
Sam Liu <hello@samliu.dev>
2024-10-01 22:12:59 -0400

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,53 +1,24 @@
 import "./App.css";
 import React, { useEffect, useRef, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { initParticlesEngine } from "@tsparticles/react";
 import { loadFull } from "tsparticles";
-import { confetti } from "@tsparticles/confetti";
 import useSound from "use-sound";
-
-import {
-  DndContext,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  sortableKeyboardCoordinates,
-  useSortable,
-} from "@dnd-kit/sortable";
-import { CSS as dndCSS } from "@dnd-kit/utilities";
-
-import faqData from "./faqData";
-
-import faq1 from "./art/faqbkgr/faq1.png";
-import faq2 from "./art/faqbkgr/faq2.png";
-import faq3 from "./art/faqbkgr/faq3.png";
-import faq4 from "./art/faqbkgr/faq4.png";
-import faq5 from "./art/faqbkgr/faq5.png";
-import faq6 from "./art/faqbkgr/faq6.png";
-import faq7 from "./art/faqbkgr/faq7.png";
-import faq8 from "./art/faqbkgr/faq8.png";
 
 import Hero from "./components/Hero";
 import Locations from "./components/Locations";
 import Cards from "./components/Cards";
 import Steps from "./components/Steps";
 import About from "./components/About";
+import Faq from "./components/Faq";
 
 import music from "./music.mp3";
-
-const faqBkgrs = [faq1, faq2, faq3, faq4, faq5, faq6, faq7, faq8];
+import Footer from "./components/Footer";
 
 export default function App() {
   const [init, setInit] = useState(false);
   const [docHeight, setDocHeight] = useState(0);
   const [scrollPos, setScrollPos] = useState(0);
-  const [screenHeight, setScreenHeight] = useState(0);
   const [isMuted, setIsMuted] = useState(true);
-  const [hasPlayed, setHasPlayed] = useState(false);
 
   const [play, { stop }] = useSound(music, { volume: isMuted ? 0 : 1 });
 
@@ -57,7 +28,6 @@ export default function App() {
     }
 
     setDocHeight(document.documentElement.scrollHeight);
-    setScreenHeight(window.innerHeight);
 
     initParticlesEngine(async (engine) => {
       await loadFull(engine);
@@ -72,11 +42,6 @@ export default function App() {
   }, []);
 
   const handleScroll = () => {
-    // if (!hasPlayed) {
-    //   play();
-    //   setIsMuted(false);
-    //   setHasPlayed(true);
-    // }
     setScrollPos(window.scrollY);
   };
 
@@ -90,10 +55,6 @@ export default function App() {
     }
     setIsMuted(!isMuted);
   }
-
-  // useEffect(() => {
-  //   console.log(docHeight - scrollPos);
-  // }, [scrollPos]);
 
   return (
     <div>
@@ -171,25 +132,6 @@ export default function App() {
 
         <Locations />
 
-        {/* <Organize /> */}
-
-        {/* <div class="border-t-4 border-pink border-dashed"></div> */}
-
-        {/* <div className="relative">
-          <img
-            src={flare9}
-            alt=""
-            className="absolute -z-10 -mt-24 sm:-mt-40 lg:mt-0 w-[200px] sm:w-[300px] xl:w-[400px] h-auto -left-12 sm:left-0"
-          />
-        </div>
-        <div className="relative">
-          <img
-            src={flare10}
-            alt=""
-            className="absolute -z-10 -mt-24 sm:-mt-32 lg:mt-0 w-[200px] sm:w-[300px] xl:w-[400px] h-auto -right-12 sm:right-0"
-          />
-        </div> */}
-
         <div
           class="faq"
           className="flex flex-col justify-center text-center retro py-12"
@@ -201,77 +143,11 @@ export default function App() {
             <div class="flex justify-center">
               <Faq />
             </div>
-            {/*<p className="text-xl">
-              Drag around the FAQ boxes to solve a puzzle!
-            </p>*/}
           </div>
         </div>
       </div>
 
-      {/*<div className="neuebit my-20 mx-5">
-          <div className="flex justify-center items-center text-center">
-            <p className="text-3xl mb-4">
-              Photos from past Hack Club hackathons:
-            </p>{" "}
-          </div>
-          <div>
-
-          </div>
-
-      </div> */}
-
-      <div className="mt-4 p-6 bg-pink text-2xl neuebit">
-        <div className="text-center">
-          <p>Built with love by teenagers, for teenagers at Hack Club</p>
-        </div>
-        <div className="flex justify-center items-center ">
-          <a
-            href="https://hackclub.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-4"
-          >
-            Hack Club
-          </a>{" "}
-          |{" "}
-          <a
-            href="https://hackclub.com/slack/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-4"
-          >
-            Slack
-          </a>{" "}
-          |{" "}
-          <a
-            href="https://www.youtube.com/@HackClubHQ"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-4"
-          >
-            YouTube
-          </a>{" "}
-          |{" "}
-          <a
-            href="https://www.instagram.com/starthackclub/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-4"
-          >
-            Instagram
-          </a>
-        </div>
-        <div className="text-center">
-          <a
-            href="https://www.instagram.com/aeralixe/?hl=en"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-2"
-          >
-            Art by Elena Baskakova (18, Boston)
-          </a>
-        </div>
-      </div>
+      <Footer />
 
       <svg width="0" height="0" className="block">
         <filter id="grainy" x="0" y="0" width="100%" height="100%">
@@ -282,113 +158,4 @@ export default function App() {
       </svg>
     </div>
   );
-}
-
-const FaqCard = (props) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: props.id });
-
-  const faqBkgr = props.bkgrs ? faqBkgrs[props.bkgrs[props.id - 1] - 1] : null;
-
-  const style = {
-    transform: dndCSS.Transform.toString(transform),
-    transition,
-    backgroundImage: faqBkgr ? `url(${faqBkgr})` : undefined,
-  };
-
-  const faqItem = faqData[props.id - 1];
-
-  return (
-    <div
-      className="bg-pink max-w-lg lg:w-[445px] lg:h-[280px] bg-cover bg-center"
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-    >
-      <div className="p-5 h-full">
-        <div className="h-full bg-black/40 p-3 flex flex-col justify-center">
-          <p className="mb-2 text-lg uppercase retro">{faqItem.question}</p>
-          <p
-            className="neuebit text-xl text-justify leading-6"
-            dangerouslySetInnerHTML={{ __html: faqItem.answer }}
-          ></p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-function Faq() {
-  const numFaq = faqData.length;
-  const faqIds = Array.from({ length: numFaq }, (_, i) => i + 1);
-  const [faqItems, setFaqItems] = useState(faqIds);
-  const [randomFaqBkgrs, setRandomFaqBkgrs] = useState([]);
-  const [screenWidth, setScreenWidth] = useState(0);
-  const [correctOrder, setCorrectOrder] = useState([]);
-  const [prevSolved, setPrevSolved] = useState(false);
-
-  useEffect(() => {
-    setScreenWidth(window.innerWidth);
-    const randArray = Array.from({ length: numFaq }, (_, i) => i + 1).sort(
-      () => Math.random() - 0.5
-    );
-    setRandomFaqBkgrs(randArray);
-    const correct = faqIds.toSorted(
-      (a, b) => randArray[a - 1] - randArray[b - 1]
-    );
-    setCorrectOrder(correct);
-  }, []);
-
-  useEffect(() => {
-    if (
-      !prevSolved &&
-      JSON.stringify(faqItems) === JSON.stringify(correctOrder)
-    ) {
-      confetti({
-        particleCount: 300,
-        spread: 160,
-        origin: { y: 1 },
-        startVelocity: 75,
-        colors: ["#65FF96", "#43DDFF", "#2A2AFF", "#FF4387"],
-      });
-      setPrevSolved(true);
-    }
-  }, [faqItems, correctOrder]);
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-  return (
-    <div className="p-5 gap-8 inter grid lg:grid-cols-2">
-      {screenWidth > 1024 ? (
-        <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
-          <SortableContext items={faqItems}>
-            {faqItems.map((id) => (
-              <FaqCard id={id} key={id} bkgrs={randomFaqBkgrs} />
-            ))}
-          </SortableContext>
-        </DndContext>
-      ) : (
-        faqItems.map((id) => <FaqCard id={id} key={id} />)
-      )}
-    </div>
-  );
-
-  function handleDragEnd(event) {
-    const { active, over } = event;
-
-    if (active.id !== over.id) {
-      setFaqItems((items) => {
-        const oldIndex = items.indexOf(active.id);
-        const newIndex = items.indexOf(over.id);
-
-        return arrayMove(items, oldIndex, newIndex);
-      });
-    }
-  }
 }
