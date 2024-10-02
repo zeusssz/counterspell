07fdcfa72e472565670f7dae16043709af79d167
Karthik Sankar <emergenitro@gmail.com>
2024-10-02 13:08:20 +0800

    imports fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./pages/App";
 import NotFound from "./pages/NotFound";
