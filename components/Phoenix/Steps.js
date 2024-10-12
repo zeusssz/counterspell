3662941531d1e99d9a38b25a7d604e7c1790c6f8
pizzalover125 <119770527+pizzalover125@users.noreply.github.com>
2024-10-12 07:51:03 -0700

    Update Steps.js

--- components/Phoenix/Steps.js
+++ components/Phoenix/Steps.js
@@ -34,10 +34,8 @@ export default function Steps() {
           </div>
           <div className="py-10 mx-6 space-y-4 text-xl uppercase retro">
             <p>
-              {/* TODO: Change `Example City` to the name of your city */}
-              Counterspell Example City will go on for{" "}
-              {/* TODO: Change `24 hours` to match your event duration */}
-              <span className="text-pink">24 hours</span>
+              Counterspell Phoenix will go on for{" "}
+              <span className="text-pink">12 hours</span>
             </p>
             <p>Here's a rough schedule so you know what to expect!</p>
           </div>
