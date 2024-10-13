f5d79c089086a102ad355419b7c14fdcd53a9739
tamzid10001 <tamzid@bloodlinkfoundation.com>
2024-10-13 15:48:11 +0600

    Update Steps.js

--- components/Dhaka/Steps.js
+++ components/Dhaka/Steps.js
@@ -12,7 +12,8 @@ export default function Steps() {
             <div className="flex flex-wrap items-center justify-center gap-6 p-8 border-4 border-dashed border-pink inter">
               <StepCard
                 img="/photos/3.png"
-                text="Sign up for a Counterspell Event in your city"
+                // TODO: Change `Example City` to the name of your city
+                text="Sign up for Counterspell Dhaka"
               />
               <StepCard
                 img="/photos/4.png"
@@ -34,9 +35,10 @@ export default function Steps() {
           </div>
           <div className="py-10 mx-6 space-y-4 text-xl uppercase retro">
             <p>
-              Most Counterspell events will go on for{" "}
-              <span className="text-pink">24 hours</span> (unless explicitly
-              stated)
+              {/* TODO: Change `Example City` to the name of your city */}
+              Counterspell Dhaka will go on for{" "}
+              {/* TODO: Change `24 hours` to match your event duration */}
+              <span className="text-pink">24 hours</span>
             </p>
             <p>Here's a rough schedule so you know what to expect!</p>
           </div>
