70d774a2668762d5cf111403fa57312d00e75e43
tamzid10001 <tamzid@bloodlinkfoundation.com>
2024-10-13 15:45:20 +0600

    Update Locations.js

--- components/Dhaka/Locations.js
+++ components/Dhaka/Locations.js
@@ -1,6 +1,7 @@
 import Subscribe from "../Subscribe";
 
 export default function Locations() {
+  // Do not update this list! We'll update it for you.
   const cities = [
     "Boston, MA",
     "New York City, NY",
@@ -11,10 +12,17 @@ export default function Locations() {
     "Burlington, VT",
     "Oshkosh, WI",
   ];
+  // Do not update this list! We'll update it for you.
 
   return (
     <div className="relative px-6 py-24 text-center neuebit bg-darker retro">
-      <p className="text-3xl uppercase">It's happening all around you</p>
+      <p className="text-3xl uppercase">
+        {/* TODO: Replace `Example City` with your city */}
+        Can't make it to Dhaka?
+      </p>
+      <p className="text-xl">
+        There are 200+ other Counterspell locations worldwide!
+      </p>
       <div className="flex justify-center pt-24">
         <div className="grid items-center justify-center w-full max-w-5xl grid-cols-1 gap-8 text-2xl lg:grid-cols-2">
           {cities.map((city, i) => (
@@ -27,16 +35,19 @@ export default function Locations() {
 
       <div className="flex justify-center my-20 space-y-2 text-2xl text-center">
         <div className="px-6 py-6 text-4xl tracking-wider border-4 border-dashed border-pink neuebit">
-          <p>Counterspell is organized by teenagers, for teenagers.</p>
           <p>
-            Don't see your city on the list? Sign up to{" "}
+            {/* TODO: Replace `Example City` with your city */}
+            Counterspell Dhaka is organized by teenagers, for teenagers.
+          </p>
+          <p>
+            Organize a Counterspell event in your city.{" "}
             <a
               href="https://hack.club/counterspell-signup"
               target="_blank"
               rel="noreferrer"
               className="text-pink"
             >
-              organize here
+              Sign up
             </a>
             .
           </p>
@@ -47,7 +58,8 @@ export default function Locations() {
         <p className="text-xl text-center">
           Get notified when registrations open
         </p>
-        <Subscribe />
+        {/* TODO: Replace `Example City` with your city */}
+        <Subscribe eventName="Dhaka" />
       </div>
     </div>
   );
