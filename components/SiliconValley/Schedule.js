08288549b287d8e64cb5ee45f393a5e0c4025824
Adam Xu <contact@adamxu.net>
2024-10-12 21:52:13 -0700

    added schedule

--- components/SiliconValley/Schedule.js
+++ components/SiliconValley/Schedule.js
@@ -1,33 +1,43 @@
+import React, { useState, useEffect } from "react";
+
 export default function Schedule() {
-  // TODO: Edit this to match your event schedule
-  const schedule = [
-    { time: "11:00 AM", event: "Doors open" },
-    { time: "12:00 PM", event: "Opening ceremony" },
-    { time: "12:30 PM", event: "Lunch" },
-    { time: "1:00 PM", event: "Start working on your game!" },
-    { time: "2:00 PM", event: "Gamedev workshop 1" },
-    { time: "4:00 PM", event: "Activity 1" },
-    { time: "4:00 PM", event: "Gamedev workshop 2" },
-    { time: "6:00 PM", event: "Dinner" },
-    { time: "8:00 PM", event: "Lightning talks" },
-    { time: "12:00 AM", event: "Midnight surprise" },
-    { time: "8:00 AM", event: "Breakfast" },
-    { time: "10:30 AM", event: "Game demos!" },
-    { time: "12:00 PM", event: "Closing ceremony" },
-  ];
+  const [schedule, setSchedule] = useState([]);
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      try {
+        const timestamp = new Date().getTime();
+        const response = await fetch(
+          "https://adamxu.net/counterspell/schedule.json?t=${timestamp}"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch schedule");
+        }
+        const data = await response.json();
+        setSchedule(data);
+      } catch (error) {
+        console.error("Error fetching schedule:", error);
+        // Optionally, set a default schedule or show an error message
+      }
+    };
+
+    fetchSchedule();
+  }, []);
 
   return (
     <>
+      <h2 className="text-center text-4xl uppercase retro text-whitepurp">
+        Schedule
+      </h2>
       <div className="flex flex-col items-center neuebit">
         <div className="flex flex-col w-full max-w-3xl px-12 pb-12 space-y-2 text-xl sm:text-4xl lg:text-5xl lg:max-w-4xl lg:space-y-3">
-          <h1 style={{ alignSelf: "center" }}>Schedule coming soon!</h1>
-          {/* {schedule.map((item, i) => (
+          {schedule.map((item, i) => (
             <div className="flex" key={i}>
               <p>{item.event}</p>
               <div className="mx-4 -translate-y-3 border-b-4 border-dotted grow"></div>
               <p>{item.time}</p>
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
     </>
