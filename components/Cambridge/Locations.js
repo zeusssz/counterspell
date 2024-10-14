6ac7d23ad07e8a9ceb9d4002d53131d2e4c87cff
mattsoh <mattsoh100@gmail.com>
2024-10-14 19:36:10 +0100

    change!

--- components/Cambridge/Locations.js
+++ components/Cambridge/Locations.js
@@ -48,7 +48,7 @@ export default function Locations() {
           rel="noopener noreferrer"
           style={{ textDecorationLine: "none" }}
         >
-            r interest in Counterspell Cambridge
+          Register interest in Counterspell Cambridge
         </a>
       </div>
     </div>
