bb41ea62132c3d7a33432b328a90280ce90d5ae3
Issac Liu <issac@matchaflavored.site>
2024-10-14 02:52:54 -0400

    Add NYC event components

---
+++ components/NYC/Locations.js
@@ -0,0 +1,68 @@
+import Subscribe from "../Subscribe";
+
+export default function Locations() {
+  // Do not update this list! We'll update it for you.
+  const cities = [
+    "Boston, MA",
+    "New York City, NY",
+    "Ottawa, ON",
+    "Basking Ridge, NJ",
+    "Austin, TX",
+    "San Francisco, CA",
+    "Burlington, VT",
+    "Oshkosh, WI",
+  ];
+  // Do not update this list! We'll update it for you.
+
+  return (
+    <div className="relative px-6 py-24 text-center neuebit bg-darker retro">
+      <p className="text-3xl uppercase">
+        {/* TODO: Replace `Example City` with your city */}
+        Can't make it to New York City?
+      </p>
+      <p className="text-xl">
+        There are 200+ other Counterspell locations worldwide!
+      </p>
+      <div className="flex justify-center pt-24">
+        <div className="grid items-center justify-center w-full max-w-5xl grid-cols-1 gap-8 text-2xl lg:grid-cols-2">
+          {cities.map((city, i) => (
+            <p className={`${
+              city.includes("New York City") ? "text-pink" : ""
+            } lg:odd:text-left lg:even:text-right`} key={i}>
+              {city}
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center my-20 space-y-2 text-2xl text-center">
+        <div className="px-6 py-6 text-4xl tracking-wider border-4 border-dashed border-pink neuebit">
+          <p>
+            {/* TODO: Replace `Example City` with your city */}
+            Counterspell New York is organized by teenagers, for teenagers.
+          </p>
+          <p>
+            Organize a Counterspell event in your city.{" "}
+            <a
+              href="https://hack.club/counterspell-signup"
+              target="_blank"
+              rel="noreferrer"
+              className="text-pink"
+            >
+              Sign up
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center space-y-3 uppercase">
+        <p className="text-xl text-center">
+          Get notified when registrations open
+        </p>
+        {/* TODO: Replace `Example City` with your city */}
+        <Subscribe eventName="New York" />
+      </div>
+    </div>
+  );
+}
