87018c97fad136ffd3860a108829832817e981a5
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-10-14 23:08:26 -0400

    added import

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Subscribe(props) {
   const [submitBtn, setSubmitBtn] = useState("Sign up");
