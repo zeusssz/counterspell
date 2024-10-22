bfbc0a4920fbacbdc6c4e21f621153f6b98b7b06
Sam Liu <hello@samliu.dev>
2024-10-22 00:43:03 -0400

    Fix comment

--- pages/hongkong.js
+++ pages/hongkong.js
@@ -46,12 +46,12 @@ export default function HongKong() {
     <>
       <Head>
         <title>Counterspell Hong Kong</title>
-        <!--<script>
+        {/* <script>
           {`
             !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetGroupPropertiesForFlags resetPersonPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
             posthog.init('phc_6ZwHOIl8Q3SQVW3wyJtEeiBAEyy7wO90wE5jJEMftk1',{api_host:'https://data.anscg.workers.dev', person_profiles: 'always' });
           `}
-        </script>-->
+        </script> */}
       </Head>
       <div>
         <div
@@ -129,9 +129,7 @@ export default function HongKong() {
 
           <div className="flex flex-col justify-center py-12 text-center faq fusion-pixel">
             <div className="m-6">
-              <p className="mb-6 text-5xl uppercase">
-                常見問題
-              </p>
+              <p className="mb-6 text-5xl uppercase">常見問題</p>
               <div className="flex justify-center">
                 <Faq />
               </div>
